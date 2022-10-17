f1b5212c754caf9930199a426f0df328a59e0769
oreste <muhirwa.g.oreste@gmail.com>
2022-10-17 15:58:35 +0200

    Le constructeur doit initialiser le plateau, avec la bonne taille, et les bonne charactères libre, la fonction evalCase nous renvoi le charactère qui se trouve dans la grille a l'endroit ligne, et colonne,

--- src/Grille.java
+++ src/Grille.java
@@ -3,13 +3,24 @@ public class Grille {
     private final char libre;
     private char[][] grille;
 
+    public Grille(int nDimension, char libre) {
+        this.nDimension = nDimension;
+        this.libre = libre;//"." libre doit etre un point, une espace vide, dans un console, et un bouton avec rien dans l'interface graphique
+
+        grille = new char[nDimension][nDimension];
+        initialiserPlateau();
+    }
 
     private void initialiserPlateau (){
-        for(int i = 0; i < nDimension; ++i){
-            for(int j = 0; j < nDimension; ++j){
-                grille[i][j] = libre;
+        for(int ligne = 0; ligne < nDimension; ++ligne){
+            for(int colonne = 0; colonne < nDimension; ++colonne){
+                grille[ligne][colonne] = libre;
             }
         }
+    }
+    public char evalCase(int ligne, int colonne){
+        assert(0 <= ligne && ligne < nDimension && 0 <= colonne && colonne < nDimension);
+        return grille[ligne][colonne];
     }
 
 }
