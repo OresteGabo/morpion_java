6b253773aa4c34f92a018257951b46fbd902ded2
oreste <muhirwa.g.oreste@gmail.com>
2022-10-17 15:42:24 +0200

    la fonction initialiserPlateau va initialiser notre plateau par des cases vides, soit un point sur le console, ou un bouton sans label dans l'interface graphique

--- src/Grille.java
+++ src/Grille.java
@@ -3,4 +3,13 @@ public class Grille {
     private final char libre;
     private char[][] grille;
 
+
+    private void initialiserPlateau (){
+        for(int i = 0; i < nDimension; ++i){
+            for(int j = 0; j < nDimension; ++j){
+                grille[i][j] = libre;
+            }
+        }
+    }
+
 }
