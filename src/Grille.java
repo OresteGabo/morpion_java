091cb03c7b56bdb12dbd38a888db32c8079bef77
oreste <muhirwa.g.oreste@gmail.com>
2022-10-18 02:51:32 +0200

    Le plateau est affiche en fonction du nombre d'espaces entre les valeur, ainsi que le nombre d'espaces sur les bordures, ces meme valeurs vont determiné la taille du plateau, il est conseillé de garder la dimension du plateau inferieur à 10 pour que l'affichage soit parfait

--- src/Grille.java
+++ src/Grille.java
@@ -1,19 +1,21 @@
+
+
 public class Grille {
-    private final int nDimension;
+    private final int dimension;
     private final char libre;
     private char[][] grille;
 
-    public Grille(int nDimension, char libre) {
-        this.nDimension = nDimension;
+    public Grille(int dimension, char libre) {
+        this.dimension = dimension;
         this.libre = libre;//"." libre doit etre un point, une espace vide, dans un console, et un bouton avec rien dans l'interface graphique
 
-        grille = new char[nDimension][nDimension];
+        grille = new char[dimension][dimension];
         initialiserPlateau();
     }
 
     private void initialiserPlateau (){
-        for(int ligne = 0; ligne < nDimension; ++ligne){
-            for(int colonne = 0; colonne < nDimension; ++colonne){
+        for(int ligne = 0; ligne < dimension; ++ligne){
+            for(int colonne = 0; colonne < dimension; ++colonne){
                 grille[ligne][colonne] = libre;
             }
         }
@@ -24,13 +26,13 @@ public class Grille {
      @param ligne numéro de ligne
      @param colonne numéro de colonne
      @return la valeaur en (ligne,colonne) de la Grille
-     @throws ArrayIndexOutOfBoundsException  si la ligne ou la colonne n'est pas dans [o..nDimension[
+     @throws ArrayIndexOutOfBoundsException  si la ligne ou la colonne n'est pas dans [o..dimension[
      */
     public  char evalCase(int ligne, int colonne){
-        assert(0 <= ligne && ligne < nDimension && 0 <= colonne && colonne < nDimension);
+        assert(0 <= ligne && ligne < dimension && 0 <= colonne && colonne < dimension);
         char c;
         try{
-        c=  grille[ligne][colonne];}
+            c=  grille[ligne][colonne];}
         catch(ArrayIndexOutOfBoundsException e){
             System.out.println("ArrayIndexOutOfBoundsException , ligne ou colonne a invalide");
             c='#';
@@ -44,8 +46,8 @@ public class Grille {
      @param colonne numéro de colonne
      @param valeur la nouvelle valeur
      */
-     public void modifierCase(int ligne, int colonne, char valeur){
-        assert(0 <= ligne && ligne < nDimension && 0 <= colonne && colonne < nDimension);
+    public void modifierCase(int ligne, int colonne, char valeur){
+        assert(0 <= ligne && ligne < dimension && 0 <= colonne && colonne < dimension);
         grille[ligne][colonne] = valeur;
     }
 
@@ -56,7 +58,84 @@ public class Grille {
      @return true(vrai) si (nLigne, nColonne) est dans le plateau
      */
     public boolean dansPlateau(int nLigne, int nColonne){
-        return (0 <= nLigne && nLigne < nDimension && 0 <= nColonne && nColonne <nDimension);
+        return (0 <= nLigne && nLigne < dimension && 0 <= nColonne && nColonne <dimension);
+    }
+
+
+    /**
+     Affichage du plateau
+     @param nbEspaces nombre d'espaces entre les valeurs
+     @param nbEspacesCote nombres d'espaces à coté des valeurs
+     */
+    void afficherPlateau(int nbEspaces,int nbEspacesCote){
+        assert(nbEspaces>=0 && nbEspacesCote>=0);
+        System.out.println("             Morpion version 1.0");
+
+        //Hashtag pour le mur du haut
+        System.out.print(" ");
+        for(int x=0;x<(nbEspaces*(dimension-1))+(nbEspacesCote*2)+dimension+4;x++){
+            System.out.print("#");
+        }
+        System.out.print("\n #");
+        for(int x=0;x<nbEspacesCote+2;x++){
+            System.out.print(' ');
+        }
+        //System.out.print("\n #      ");
+        for(int x=0;x<dimension;x++){
+            System.out.print(x);
+            for(int y=0;y<((x==dimension-1)? nbEspacesCote:nbEspaces);y++){
+                System.out.print(' ');
+            }
+        }System.out.println('#');
+
+
+
+
+        //Le debut du ligne qui dessine les underscore
+        System.out.print(" # X");
+        for(int x=0;x<nbEspacesCote;x++){
+            System.out.print(' ');
+        }
+        /*
+        * nbEspaces* (dimension-1) vont nous donner ne nombre de underscores à mettre, pour couvrire les espaces entre les element du grilles
+        * (nbEspacesMin+dimension)  vont ajouter les underscores à la places des elements du grilles, ainsi que le nombre despaceMin à la fin
+        * */
+        for(int x=0;x<(nbEspaces*(dimension-1))+(dimension);x++) {
+            System.out.print("_");
+        }
+        for(int x=0;x<nbEspacesCote;x++){
+            System.out.print(' ');
+        }
+        System.out.println("#");
+        //Fin du ligne des underscore
+
+
+
+        //Début du ligne qui affiche les valeurs du grille
+        for(int i = 0; i < dimension; ++i){
+            System.out.print(" # "+i);
+            for(int x=0;x<nbEspacesCote;x++){
+                System.out.print(' ');
+            }
+            for(int j = 0; j < dimension; ++j){
+                System.out.print(grille[i][j]);
+                for(int x=0;x<((j==dimension-1)? nbEspacesCote:nbEspaces);x++){
+                    System.out.print(' ');
+                }
+            }
+            System.out.println("#");
+        }
+        //Fin du ligne qui affiche les valeurs du grille
+
+
+        //Hashtag pour le mur du bas
+        System.out.print(" ");
+        for(int x=0;x<(nbEspaces*(dimension-1))+(nbEspacesCote*2)+dimension+4;x++){
+            System.out.print("#");
+        }
+        System.out.println();
+
     }
 
 }
+
