1df41aabe4c8fcf282ad81e9d2aeb0dba977123b
oreste <muhirwa.g.oreste@gmail.com>
2022-10-20 04:37:58 +0200

    Le score doit etre incrementer quand le joeur gagne, et on doit pouvoir savoir son score, ainsi que son symbole pour l'identifier

--- src/Joueur.java
+++ src/Joueur.java
@@ -5,4 +5,28 @@ public class Joueur{
         this.symbole=symbole;
         score=0;
     }
+
+    /**
+     Quand le joueur gagne, il faut incrementer sa note.
+     */
+    public void incrementerScore(){
+        score++;
+    }
+
+    /**
+     Le symbole du joueur
+     Ce symbole sera utilisé comme étiquette sur les boutons
+     @return symbole du joueur
+     */
+    public char getSymbole() {
+        return symbole;
+    }
+
+    /**
+     La note du joueur
+     @return la note du joueur
+     */
+    public int getScore() {
+        return score;
+    }
 }
